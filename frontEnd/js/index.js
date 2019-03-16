4d8e111462e42a6573c242b877caa86cc1fc1cbb
seshanthS <seshanth.s@hotmail.com>
2019-03-16 17:35:45 +0530

    Fields added
    TODO
    1.verify() function
    2.css

--- frontEnd/js/index.js
+++ frontEnd/js/index.js
@@ -2,18 +2,14 @@ var contractAddress = "0x23c4afef9623cc762a4649aaa319b1b50aaade74";
 async function signAndSubmit(){
   //var web3 = window.web3;
   checkAndInjectProvider();
-  var acc = await getDefaultAccount();
+  var account = await getDefaultAccount();
   var name = document.getElementById('name').value
   var certNumber = document.getElementById('certificateNumber').value
   var cgpa = document.getElementById('cgpa').value
-  //============================
-  //TODO - add fields for signerId, certifiedTo
-  // calculate hash of data{} - certHash
-  var certifiedTo = "0xF8E7cFf01db79c156C6aE7e2804fbB97628f0Ef5";          
-  var signerId = 1; 
-  var certHash = window.web3.utils.asciiToHex('1');      //to be removed      
-  //============================       
-  var data ={
+  var certifiedTo = document.getElementById('addressOfStudent').value//"0xF8E7cFf01db79c156C6aE7e2804fbB97628f0Ef5";          
+  var signerId = document.getElementById('signerId').value; 
+
+  var data = {
     name :name,
     certNo:certNumber,
     cgpa: cgpa
@@ -22,7 +18,7 @@ async function signAndSubmit(){
   window.web3.eth.personal.sign(dataToSign, await getDefaultAccount()).then((signature)=>{
     var contractInstance = new window.web3.eth.Contract(abi, contractAddress);
     
-    contractInstance.methods.broadcast(certNumber, certifiedTo, signature, signerId).send({from: acc})
+    contractInstance.methods.broadcast(certNumber, certifiedTo, signature, signerId).send({from: account})
     .on('transactionHash',(txHash)=>{
       alert("transaction Hash : " + txHash );
     });
